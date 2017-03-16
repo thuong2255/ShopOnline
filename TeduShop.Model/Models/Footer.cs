6a8edee2a250b5c657bfadec4ef9fe6c7f4f07f7
DESKTOP-A027VG4\thuong <thuong2255@gmail.com>
2017-03-17 00:49:21 +0700

    Commit model

---
+++ TeduShop.Model/Models/Footer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        public string ID { set; get; }
+
+        [Required]
+        public String Content { set; get; }
+    }
+}
